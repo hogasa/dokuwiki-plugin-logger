b15351645d14b2216ff4e8a81071fef2cb902367
jens <jens@nb-ubuntu-jens>
2015-01-04 17:18:35 +0100

    Initial commit

--- conf/default.php
+++ conf/default.php
@@ -5,11 +5,11 @@
  * @author Jens Pabel <info@geofreak.de>
  */
 
-$conf['enabled'] = 0;
+$conf['enabled'] = 1;
 $conf['date'] = 1;
 $conf['time'] = 1;
 $conf['ip']  = 1;
 $conf['user'] = 1;
-$conf['pc_name'] = 0;
+$conf['pc_name'] = 1;
 $conf['query_string'] = 1;
 $conf['user_agent'] = 1;
