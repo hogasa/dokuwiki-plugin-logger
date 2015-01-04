185d0c7a54a9c581317f530895c44f2aec52d90c
jens <jens@nb-ubuntu-jens>
2015-01-04 16:31:54 +0100

    Initial commit

--- action/logging.php
+++ action/logging.php
@@ -89,57 +89,6 @@ class action_plugin_logger_logging extends DokuWiki_Action_Plugin {
             }
 
             #-----------------------------------------------------------------------
-            # http://ipinfo.io is enabled.
-            # 
-            # Example: http://ipinfo.io/91.56.234.56/json
-            #
-            if ($this->getConf('ipinfo_enabled')) {
-
-                #Request from http://ipinfo.io as json
-                $info_json_string = file_get_contents("http://ipinfo.io/" . $_SERVER['REMOTE_ADDR'] . "/json");
-
-                #Request from http://ipinfo.io as array
-                $info_array = json_decode($info_json_string, true);
-
-                #Check hostname option
-                if ($this->getConf('hostname') === 1) {
-                    $log_dataset .= ";" . '"' . $info_array['hostname'] . '"';
-                }
-
-                #Check city option
-                if ($this->getConf('city') === 1) {
-                    $log_dataset .= ";" . '"' . $info_array['city'] . '"';
-                }
-
-                #Check region option
-                if ($this->getConf('region') === 1) {
-                    $log_dataset .= ";" . '"' . $info_array['region'] . '"';
-                }
-
-                #Check country option
-                if ($this->getConf('country') === 1) {
-                    $log_dataset .= ";" . '"' . $info_array['country'] . '"';
-                }
-
-                #Check location option
-                if ($this->getConf('loc') === 1) {
-                    #Create wkt geometry
-                    $coordArray = explode(',', trim($info_array['loc']));
-                    $coord = 'POINT(' . $coordArray[1] . " " . $coordArray[0] . ')';
-                    $log_dataset .= ";" . '"' . $coord . '"';
-                }
-
-                #Check organization option
-                if ($this->getConf('org') === 1) {
-                    $log_dataset .= ";" . '"' . $info_array['org'] . '"';
-                }
-
-                #Check postal option
-                if ($this->getConf('postal') === 1) {
-                    $log_dataset .= ";" . '"' . $info_array['postal'] . '"';
-                }
-            }
-            #-----------------------------------------------------------------------
             #
             #Remove the first delimiter (;)
             $log_dataset = substr($log_dataset, 1);
