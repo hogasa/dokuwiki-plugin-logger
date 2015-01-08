102ba8327d65bff2bb86875f78bf8049ef8a9e6d
jens <jens@nb-ubuntu-jens>
2015-01-08 23:20:20 +0100

    Log dokuwiki media files requests

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -10,6 +10,6 @@ $lang['date'] = "Log date:";
 $lang['time'] = "Log time:";
 $lang['ip'] = "Log IP:";
 $lang['user'] = "Log user:";
-$lang['pc_name'] = "Log pc name:";
+$lang['host_name'] = "Log host name:";
 $lang['query_string'] = "Log query string:";
 $lang['user_agent'] = "Log user agent (browser):";
